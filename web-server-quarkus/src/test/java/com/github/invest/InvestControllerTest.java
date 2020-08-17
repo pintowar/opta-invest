9d6ff606c42d32d9b980ac61333b1d254b9d8509
Thiago Oliveira Pinheiro <pintowar@gmail.com>
2020-08-17 01:23:24 -0300

    removing tests

--- web-server-quarkus/src/test/java/com/github/invest/InvestControllerTest.java
+++ web-server-quarkus/src/test/java/com/github/invest/InvestControllerTest.java
@@ -9,13 +9,13 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class InvestControllerTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
-    }
+//    @Test
+//    public void testHelloEndpoint() {
+//        given()
+//          .when().get("/hello")
+//          .then()
+//             .statusCode(200)
+//             .body(is("hello"));
+//    }
 
 }
